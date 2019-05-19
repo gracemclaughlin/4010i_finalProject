44d9384570c82df31908228b8572a412f0ff0310
Grace McLaughlin <ghmclaughlin23@gmail.com>
2019-05-19 22:32:23 +0800

    initial commit

---
+++ stats.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import numpy
+
+import os
+import re
+import subprocess
+import tempfile
+import numpy as np
+
+from six.moves import urllib
+
+def wer(r, h):
+    """
+    This is a function that calculate the word error rate in ASR.
+    You can use it like this: wer("what is it".split(), "what is".split()) 
+    """
+    #build the matrix
+    d = numpy.zeros((len(r)+1)*(len(h)+1), dtype=numpy.uint8).reshape((len(r)+1, len(h)+1))
+    for i in range(len(r)+1):
+        for j in range(len(h)+1):
+            if i == 0: d[0][j] = j
+            elif j == 0: d[i][0] = i
+    for i in range(1,len(r)+1):
+        for j in range(1, len(h)+1):
+            if r[i-1] == h[j-1]:
+                d[i][j] = d[i-1][j-1]
+            else:
+                substitute = d[i-1][j-1] + 1
+                insert = d[i][j-1] + 1
+                delete = d[i-1][j] + 1
+                d[i][j] = min(substitute, insert, delete)
+    result = float(d[len(r)][len(h)]) / len(r) * 100
+    # result = str("%.2f" % result) + "%"
+    return result
